ce093c79eac8f12287ae5625f5530a6ef52c6809
Joel Spuds <joel.01@gmail.com>
2019-06-27 08:29:33 +0100

    adding activity log and a some other ptototype test stuff

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,17 @@ const router = express.Router()
 // import testController from './views/controllers/testController'
 
 // Add your routes here - above the module.exports line
-/*router.get('/prototypes/example-journey', function(req, res) {
-  res.render('prototypes/example-journey/index.html', { 'name' : 'Foo' });
-});*/
+router.get('/prototypes/example-journey', function(req, res) {
+  let testVar = 'blah blah ';
+  res.render('prototypes/example-journey/index.html', { 'name' : testVar });
+});
 
+
+router.post('/prototypes/example-journey', function(req, res) {
+  const { name } = req.body;
+  console.log(name);
+  return res.redirect('/prototypes/example-journey/');
+
+});
 
 module.exports = router
